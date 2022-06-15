86e81e91c9d87fbf5c4e1209f251a26cbedc3729
Isaac <106935763+ijeanty@users.noreply.github.com>
2022-06-15 00:35:16 -0400

    Update GradeCalculator.java
    Had 'input.nextDouble' when it's supposed to be 'sc.nextDouble' because the scanner wasn't resolved to a type.

--- HW1/src/GradeCalculator.java
+++ HW1/src/GradeCalculator.java
@@ -5,19 +5,19 @@ import java util.Scanner;
       Scanner sc = new Scanner(System.in); //sc variable = the input of the user
       
       System.out.print("Enter Homework score: "); //user input for Homework grade
-      double homework = input.nextDouble(); //reads the user input in whatever score it receives
+      double homework = sc.nextDouble(); //reads the user input in whatever score it receives
       
       System.out.print("Enter CodeLab score: "); //user input for CodeLab grade
-      double codelab = input.nextDouble(); //reads the user input in whatever score it receives
+      double codelab = sc.nextDouble(); //reads the user input in whatever score it receives
       
       System.out.print("Enter MidTerm 1 score: "); //user input for MidTerm 1 grade
-      double midtermOne = input.nextDouble(); //reads the user input in whatever score it receives
+      double midtermOne = sc.nextDouble(); //reads the user input in whatever score it receives
       
       System.out.print("Enter MidTerm 2 score: "); //user input for MidTerm 2 grade
-      double midtermTwo = input.nextDouble(); //reads the user input in whatever score it receives
+      double midtermTwo = sc.nextDouble(); //reads the user input in whatever score it receives
       
       System.out.print("Enter FINAL score: "); //user input for Final exam grade
-      double finalExam = input.nextDouble(); //reads the user input in whatever score it receives
+      double finalExam = sc.nextDouble(); //reads the user input in whatever score it receives
       
       //calculation of inputs
       double overall = homework + codelab + midtermOne + midtermTwo + finalExam
