97caa906ea8c5bc48f6e3cbb4728423ea0218715
Isaac <106935763+ijeanty@users.noreply.github.com>
2022-06-14 23:19:33 -0400

    Create GradeCalculator.java
    the "double" keyword is better to use than "int" because the user might have something like 90.5 as a score.

---
+++ HW1/src/GradeCalculator.java
@@ -0,0 +1,24 @@
+import java util.Scanner;
+
+  public class GradeCalculator {
+    public static void main (String[] args) {
+      Scanner sc = new Scanner(System.in); //sc variable = the input of the user
+      
+      System.out.print("Enter Homework score: "); //user input for Homework grade
+      double homework = input.nextDouble(); //reads the user input in whatever score it receives
+      
+      System.out.print("Enter CodeLab score: "); //user input for CodeLab grade
+      double homework = input.nextDouble(); //reads the user input in whatever score it receives
+      
+      System.out.print("Enter MidTerm 1 score: "); //user input for MidTerm 1 grade
+      double homework = input.nextDouble(); //reads the user input in whatever score it receives
+      
+      System.out.print("Enter MidTerm 2 score: "); //user input for MidTerm 2 grade
+      double homework = input.nextDouble(); //reads the user input in whatever score it receives
+      
+      System.out.print("Enter MidTerm 3 score: "); //user input for MidTerm 3 grade
+      double homework = input.nextDouble(); //reads the user input in whatever score it receives
+                       
+      
+      
+      
